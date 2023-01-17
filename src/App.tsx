4bd785891833c7b05df13579331227274a968d99
Paul Voronin <voron.paul@gmail.com>
2023-01-17 14:47:27 +0200

    added Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
+import { useState } from 'react';
+import { LangContext } from "./utils/LangContext";
 import { Button } from 'antd';
 
 function App() {
+  const [lang, setLang] = useState('en-En');
   return (
-    <div className="App">
-      <h1>Test</h1>
-      <Button type="primary">Press me</Button>
-    </div>
+    <LangContext.Provider value={lang}>
+      <div className="App">
+        <h1>Test</h1>      
+        <Button type="primary">Press me</Button>
+      </div>
+    </LangContext.Provider>
   );
 }
 
